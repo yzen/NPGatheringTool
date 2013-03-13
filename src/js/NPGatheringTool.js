7ba4f851ff5450036a2213f5ff308037a6e8ca9f
Yura Zenevich <yura.zenevich@gmail.com>
2013-03-13 14:22:52 -0400

    GPII-92: Added a first pass on client side NPGatheringTool rendering component.

--- src/js/NPGatheringTool.js
+++ src/js/NPGatheringTool.js
@@ -10,8 +10,30 @@ You may obtain a copy of the License at
 https://github.com/gpii/universal/LICENSE.txt
 */
 
-(function () {
+(function ($, fluid) {
 
     "use strict";
+
+    fluid.registerNamespace("gpii");
+
+    fluid.defaults("gpii.NPGatheringTool", {
+        gradeNames: ["autoInit", "fluid.rendererComponent"],
+        selectors: {
+            linuxGroupLabel: ".gpii-NPGatheringTool-linuxGroupLabel"
+        },
+        strings: {
+            linuxGroupLabel: "Linux"
+        },
+        renderOnInit: true,
+        protoTree: {
+            linuxGroupLabel: {
+                messagekey: "linuxGroupLabel"
+            }
+        }
+    });
+
+    $(document).ready(function () {
+        gpii.NPGatheringTool(".gpii-NPGatheringTool");
+    });
     
-})();
+})(jQuery, fluid);
